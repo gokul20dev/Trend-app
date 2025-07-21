8dd3f64147af9f860b8317d933e022f16925b077
gokul <gopalakrishnan93843@gmail.com>
2025-07-21 15:11:01 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
-# output "ec2_us_east_1_public_ip" {
-#   value = aws_instance.ec2_use1.public_ip
-# }
-
-# output "ec2_ap_south_1_public_ip" {
-#   value = aws_instance.ec2_aps1.public_ip
-# }
+ output "ec2_ap_south_1_public_ip" {
+   value = aws_instance.ec2_aps1.public_ip
+ }
