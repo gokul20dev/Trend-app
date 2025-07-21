f0ca83f219c290c4d0232629fee72d1e70de4e7e
gokul <gopalakrishnan93843@gmail.com>
2025-07-21 15:09:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,88 +1,132 @@
-# provider "aws" {
-#   alias  = "use1"
-#   region = "us-east-1"
-# }
+provider "aws" {
+  region = "ap-south-1" 
+}
 
-# provider "aws" {
-#   alias  = "aps1"
-#   region = "ap-south-1"
-# }
+# VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
 
-# # Security Group for us-east-1
-# resource "aws_security_group" "allow_http_use1" {
-#   provider    = aws.use1
-#   name        = "allow_http_use1"
-#   description = "Allow HTTP traffic"
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
 
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+# Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  tags = {
+    Name = "public-subnet"
+  }
+}
 
-# # Security Group for ap-south-1
-# resource "aws_security_group" "allow_http_aps1" {
-#   provider    = aws.aps1
-#   name        = "allow_http_aps1"
-#   description = "Allow HTTP traffic"
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
 
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
 
-# # EC2 in us-east-1
-# resource "aws_instance" "ec2_use1" {
-#   provider                  = aws.use1
-#   ami                       = "ami-09e6f87a47903347c"
-#   instance_type             = "var.instance_type"
-  
-  
-#   associate_public_ip_address = true
-#   vpc_security_group_ids    = [aws_security_group.allow_http_use1.id]
-#   user_data = file("script.sh")
-#   tags = {
-#     Name = "EC2-US-EAST-1"
-#   }
-# }
+# Security Group
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Allow SSH and Jenkins"
+  vpc_id      = aws_vpc.main_vpc.id
 
-# # EC2 in ap-south-1
-# resource "aws_instance" "ec2_aps1" {
-#   provider                  = aws.aps1
-#   ami                       = "ami-0b09627181c8d5778"
-#   instance_type             = "t2.micro"
-#   key_name = "keyaws"
-#   associate_public_ip_address = true
-#   vpc_security_group_ids    = [aws_security_group.allow_http_aps1.id]
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   user_data = file("script.sh")
+  ingress {
+    description = "Jenkins UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags = {
-#     Name = "EC2-AP-SOUTH-1"
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
+
+# IAM Role for EC2
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_jenkins_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_instance" "jenkins_server" {
+  ami                         = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  instance_type               = "t2.m"
+  subnet_id                   = aws_subnet.public_subnet.id
+  key_name                    = "aws-key" # Replace with your key name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y java-1.8.0-openjdk
+              wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+              rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+              yum install -y jenkins git
+              systemctl enable jenkins
+              systemctl start jenkins
+              EOF
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+
+# IAM Instance Profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.ec2_role.name
+}
